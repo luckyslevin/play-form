7f213be01b0d0d7b5db3bbb879bc79bbcec7cb03
Ryo Ejima <eji.san@mac.com>
2015-12-07 18:55:00 +0800

    Add comments for documentation

--- app/ejisan/play/data/validation/Constraints.scala
+++ app/ejisan/play/data/validation/Constraints.scala
@@ -3,17 +3,25 @@ package ejisan.play.data.validation
 import scala.util.matching.Regex
 import play.api.data.validation._
 
-object Constraints extends play.api.data.validation.Constraints {
+object Constraints
+  extends play.api.data.validation.Constraints
+  with HelperConstraints
+  with DateTimeConstraints
+  with TextPatternConstraints
+  with JapaneseConstraints
 
+trait HelperConstraints {
   /**
    * Defines a‘required’ constraint for `Char` values, i.e. one in which empty char is invalid.
    *
    * '''name'''[constraint.required]
    * '''error'''[error.required]
    */
-  def nonEmptyChar: Constraint[Char] = Constraint[Char]("constraint.required") { o =>
-    if (o == null) Invalid(ValidationError("error.required")) else Valid
+  final val nonEmptyChar: Constraint[Char] = Constraint[Char]("constraint.required") { o =>
+    if (o == null) Invalid(ValidationError("error.required"))
+    else Valid
   }
+
 
   /**
    * Defines a‘required’ constraint for `Option[T]` values.
@@ -21,17 +29,72 @@ object Constraints extends play.api.data.validation.Constraints {
    * '''name'''[constraint.required]
    * '''error'''[error.required]
    */
-  def some[T]: Constraint[Option[T]] = Constraint[Option[T]]("constraint.required") { o =>
-    if (o == null) Invalid(ValidationError("error.required")) else if (o.isEmpty) Invalid(ValidationError("error.required")) else Valid
+  final def some[T]: Constraint[Option[T]] = Constraint[Option[T]]("constraint.required") { o =>
+    if (o == null) Invalid(ValidationError("error.required"))
+    else if (o.isEmpty) Invalid(ValidationError("error.required"))
+    else Valid
   }
 
+
+  /**
+   * Defines a `username` constraint for `String` typed username values.
+   *
+   * '''name'''[constraint.username]
+   * '''error'''[error.username]
+   */
+  final val username: Constraint[String] = username()
+
+  /**
+   * Defines a `username` constraint for `String` typed username values.
+   *
+   * '''name'''[constraint.username]
+   * '''error'''[error.username]
+   *
+   * @param regex for username.
+   */
+  final def username(regex: Regex = """^[a-zA-Z0-9][a-zA-Z0-9\.!#$%&'*+/=?^_`{|}~-]*$""".r): Constraint[String]
+    = Constraints.pattern(regex, "constraint.username", "error.username")
+
+
+  /**
+   * Defines a `password` constraint for `String` typed password values.
+   *
+   * '''name'''[constraint.password]
+   * '''error'''[error.password]
+   */
+  final val password: Constraint[String] = password()
+
+  /**
+   * Defines a `password` constraint for `String` typed password values.
+   *
+   * '''name'''[constraint.password]
+   * '''error'''[error.password]
+   *
+   * @param hasUpperCase  A password has upper cased text or not.
+   * @param hasSymbolical A password has symbolical text or not.
+   * @param hasDigit      A password has digit or not.
+   * @param minLength     Minimum length for a password.
+   */
+  final def password(
+    hasUpperCase: Boolean = true, hasSymbolical: Boolean = true, hasDigit: Boolean = true, minLength: Int = 8): Constraint[String] = {
+    val upperCase = if (hasUpperCase) """(?=.*[A-Z])""" else ""
+    val symbolical = if (hasSymbolical) """(?=.*[ -/:-@\[-\`\{-\~])""" else ""
+    val digit = if (hasDigit) """(?=.*[0-9])""" else ""
+    Constraints.pattern(
+      s"""^.*(?=.{$minLength,})(?=.*[a-z])${upperCase}${symbolical}${digit}(?=.*).*$$""".r,
+      "constraint.password",
+      "error.password")
+  }
+}
+
+trait DateTimeConstraints {
   /**
    * Defines a `year` constraint for `Int` typed year values.
    *
    * '''name'''[constraint.year]
    * '''error'''[error.year]
    */
-  def year: Constraint[Int] = Constraint[Int]("constraint.year") { o =>
+  final val year: Constraint[Int] = Constraint[Int]("constraint.year") { o =>
     if (o == null) Invalid(ValidationError("error.year")) else try {
       val year = o.toInt
       if (year > 0 && year < 10000) Valid
@@ -47,7 +110,7 @@ object Constraints extends play.api.data.validation.Constraints {
    * '''name'''[constraint.month]
    * '''error'''[error.month]
    */
-  def month: Constraint[Int] = Constraint[Int]("constraint.month") { o =>
+  final val month: Constraint[Int] = Constraint[Int]("constraint.month") { o =>
     if (o == null) Invalid(ValidationError("error.month")) else try {
       val month = o.toInt
       if (month > 0 && month <= 12) Valid
@@ -63,7 +126,7 @@ object Constraints extends play.api.data.validation.Constraints {
    * '''name'''[constraint.day]
    * '''error'''[error.day]
    */
-  def day: Constraint[Int] = Constraint[Int]("constraint.day") { o =>
+  final val day: Constraint[Int] = Constraint[Int]("constraint.day") { o =>
     if (o == null) Invalid(ValidationError("error.day")) else try {
       val day = o.toInt
       if (day > 0 && day <= 31) Valid
@@ -79,7 +142,7 @@ object Constraints extends play.api.data.validation.Constraints {
    * '''name'''[constraint.hour]
    * '''error'''[error.hour]
    */
-  def hour: Constraint[Int] = Constraint[Int]("constraint.hour") { o =>
+  final val hour: Constraint[Int] = Constraint[Int]("constraint.hour") { o =>
     if (o == null) Invalid(ValidationError("error.hour")) else try {
       val hour = o.toInt
       if (hour >= 0 && hour < 24) Valid
@@ -95,7 +158,7 @@ object Constraints extends play.api.data.validation.Constraints {
    * '''name'''[constraint.minute]
    * '''error'''[error.minute]
    */
-  def minute: Constraint[Int] = Constraint[Int]("constraint.minute") { o =>
+  final val minute: Constraint[Int] = Constraint[Int]("constraint.minute") { o =>
     if (o == null) Invalid(ValidationError("error.minute")) else try {
       val minute = o.toInt
       if (minute >= 0 && minute < 60) Valid
@@ -111,7 +174,7 @@ object Constraints extends play.api.data.validation.Constraints {
    * '''name'''[constraint.second]
    * '''error'''[error.second]
    */
-  def second: Constraint[Int] = Constraint[Int]("constraint.second") { o =>
+  final val second: Constraint[Int] = Constraint[Int]("constraint.second") { o =>
     if (o == null) Invalid(ValidationError("error.second")) else try {
       val second = o.toInt
       if (second >= 0 && second < 60) Valid
@@ -120,53 +183,53 @@ object Constraints extends play.api.data.validation.Constraints {
       case _: Throwable => Invalid(ValidationError("error.second"))
     }
   }
+}
 
-  val username: Constraint[String] = username()
+trait TextPatternConstraints {
+  /**
+   * Defines a `ASCII` constraint for `String` typed ASCII values.
+   *
+   * '''name'''[constraint.ascii]
+   * '''error'''[error.ascii]
+   */
+  final val ascii: Constraint[String]
+    = Constraints.pattern("""^[\\u0000-\\u007F]*$""".r, "constraint.ascii", "error.ascii")
 
-  def username(regex: Regex = """^[a-zA-Z0-9][a-zA-Z0-9\.!#$%&'*+/=?^_`{|}~-]*$""".r): Constraint[String]
-    = pattern(regex, "constraint.username", "error.username")
+  /**
+   * Defines a `alphabetical` constraint for `String` typed alphabetical values.
+   *
+   * '''name'''[constraint.alphabetical]
+   * '''error'''[error.alphabetical]
+   */
+  final val alphabetical: Constraint[String]
+    = Constraints.pattern("""^[a-zA-Z ]*$""".r, "constraint.alphabetical", "error.alphabetical")
 
-  val password: Constraint[String] = password()
+  /**
+   * Defines a `symbolical` constraint for `String` typed symbolical values.
+   *
+   * '''name'''[constraint.symbolical]
+   * '''error'''[error.symbolical]
+   */
+  final val symbolical: Constraint[String]
+    = Constraints.pattern("""^[ -/:-@\[-\`\{-\~]*$""".r, "constraint.symbolical", "error.symbolical")
 
-  def password(
-    hasUpperCase: Boolean = true, hasSymbolical: Boolean = true, hasDigit: Boolean = true, minLength: Int = 8): Constraint[String] = {
-    val upperCase = if (hasUpperCase) """(?=.*[A-Z])""" else ""
-    val symbolical = if (hasSymbolical) """(?=.*[ -/:-@\[-\`\{-\~])""" else ""
-    val digit = if (hasDigit) """(?=.*[0-9])""" else ""
-    pattern(
-      s"""^.*(?=.{$minLength,})(?=.*[a-z])${upperCase}${symbolical}${digit}(?=.*).*$$""".r,
-      "constraint.password",
-      "error.password")
-  }
+  /**
+   * Defines a `digit` constraint for `String` typed digit values.
+   *
+   * '''name'''[constraint.digit]
+   * '''error'''[error.digit]
+   */
+  final val digit: Constraint[String]
+    = Constraints.pattern("""^[0-9]*$""".r, "constraint.digit", "error.digit")
 
-  val nonSpace: Constraint[String]
-    = pattern("""[^\s]+""".r, "constraint.nonSpace", "error.nonSpace")
-
-  val ascii: Constraint[String]
-    = pattern("""^[\\u0000-\\u007F]*$""".r, "constraint.ascii", "error.ascii")
-
-  val alphabetical: Constraint[String]
-    = pattern("""^[a-zA-Z ]*$""".r, "constraint.alphabetical", "error.alphabetical")
-
-  val symbolical: Constraint[String]
-    = pattern("""^[ -/:-@\[-\`\{-\~]*$""".r, "constraint.symbolical", "error.symbolical")
-
-  val digit: Constraint[String]
-    = pattern("""^[0-9]*$""".r, "constraint.digit", "error.digit")
-
-  def url(protocols: String = "https?|http|ftp|ftps|file"): Constraint[String] = pattern(
+  /**
+   * Defines a `url` constraint for `String` typed url values.
+   *
+   * '''name'''[constraint.url]
+   * '''error'''[error.url]
+   */
+  final def url(protocols: String = "https?|http|ftp|ftps|file"): Constraint[String] = Constraints.pattern(
     s"""^($protocols)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]""".r,
     "constraint.url",
     "error.url")
-
-  val httpUrl: Constraint[String] = url("http")
-
-  val httpsUrl: Constraint[String] = url("https")
-
-  val ftpUrl: Constraint[String] = url("ftp")
-
-  val ftpsUrl: Constraint[String] = url("ftps")
-
-  val fileUrl: Constraint[String] = url("file")
-
 }
